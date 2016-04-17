9239931bf71d9bdde7afa1893c35bf19f0d6f591
shopperstopdemo <admin@shopperstopdemo.in>
2016-04-17 22:09:20 +0530

    changes

--- main.js
+++ main.js
@@ -10,15 +10,12 @@ $(document).ready(function () {
     "use strict";
     $("#btnSubmit").click(function (e) {
         var settings = {
-            "async": true,
-            "crossDomain": true,
             "url": "https://api.postmarkapp.com/email/batch",
             "method": "POST",
             "headers": {
                 "x-postmark-server-token": "c48030f4-5f0a-4726-a5b4-cf9ff2661b49",
                 "content-type": "application/json",
-                "cache-control": "no-cache",
-                "postman-token": "a46d63d9-8442-0fee-38df-0ceaff392a63"
+                "cache-control": "no-cache"
             },
             "data": "[{From: 'admin@shopperstopdemo.in', To: 'saurabhdutta.jk@gmail.com', Subject: 'We have an offer you can\\'t refuse', HtmlBody: '<html> <img src=\"http://res.cloudinary.com/dren4jgbp/image/upload/c_scale,h_1400/v1460849665/ADITYA_SIR_grihhr.jpg\"/></html>'},{From: 'admin@shopperstopdemo.in', To: 'kumaraditya13@gmail.com', Subject: 'We have an offer you can\\'t refuse', HtmlBody: '<html><body> <img src=\"http://res.cloudinary.com/dren4jgbp/image/upload/c_scale,h_1400/v1460849665/ADITYA_SIR_grihhr.jpg\"/></body></html>'}]"
         }
